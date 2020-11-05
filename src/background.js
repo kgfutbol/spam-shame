719a3601fd0b4333b2f7bd5b0c190647c53f8709
Kevin Germain <kgermai1@uncc.edu>
2020-11-05 15:19:33 -0500

    Added Content.js

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 console.log('Background Execution')
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({
-        file: "./foreground"
+        file: "./foreground.js"
     });
 });
