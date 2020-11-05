185e30e87d8c03d774be0ad2d35d78662a23edb4
Stefan Dybka <sdybka@uncc.edu>
2020-11-05 17:16:41 -0500

    Added example account listing

--- src/popup.js
+++ src/popup.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("new-dot-btn").addEventListener('click', onclickNewEmail, false)
     document.getElementById("options-btn").addEventListener('click', onclickOptions, false)
+    let accountList = document.getElementById("tbody");
 
     function onclickNewEmail ()
     {
@@ -17,4 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
     {
         chrome.tabs.create({url: '../options.html'})
     }
+
+    function constructAccounts(accounts) {
+        for (var account in accounts) {
+            let row = accountList.insertRow();
+            let email = row.insertCell(0);
+            let website = row.insertCell(1);
+
+            email.innerHTML = account;
+            website.innerHTML = accounts[account];
+        }
+    }
+
+    const exampleAccounts = {"sdybka@uncc.edu" : "Facebook.com", "sdybk.a@uncc.edu" : "gmail.com"};
+    constructAccounts(exampleAccounts);
 })
