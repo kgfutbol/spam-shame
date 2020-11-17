966dfeb7db7ab23e130c054f62ec2c15866401c0
Stefan Dybka <sdybka@uncc.edu>
2020-11-17 15:24:08 -0500

    Finalized initial landing popup

--- src/initial.js
+++ src/initial.js
@@ -1,13 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("emailForm").addEventListener("submit", function () {
-        let email = document.getElementById("email").value;
-        let re = /@gmail.com$|@googlemail.com$/;
+import dialogPolyfill from "./dialog-polyfill.esm.js";
 
-        if (email.match(re) != null) {
-            chrome.storage.local.set({ "email": email }, (res) => {
-                chrome.browserAction.setPopup({ popup: '../popup.html' });
-                window.close();
+document.addEventListener("DOMContentLoaded", function () {
+    'use strict';
+    let dialogs = document.querySelectorAll('dialog');
+    dialogs.forEach(function (dialog) {
+        if (!dialog.showModal) {
+            dialogPolyfill.registerDialog(dialog);
+            console.log(dialog.id + ": registration complete");
+        }
+        dialog.querySelector('.close').addEventListener('click', function () {
+            dialog.close();
+            if (dialog.id === "successDialog") window.close();
+        });
+    });
+
+    document.getElementById("emailForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        if (validateEmail(email)) {
+            chrome.storage.local.set({"email": email}, () => {
+                chrome.browserAction.setPopup({popup: '../popup.html'});
+                document.querySelector("#successDialog").showModal();
             });
-        } else alert("This extension only supports standard google emails. Please enter a gmail.com or googlemail.com.");
+        }
     });
 });
+
+function validateEmail(email) {
+    let re = /@gmail.com$|@googlemail.com$/;
+
+    if (email.match(re) != null) {
+        console.log("Valid email");
+        return true;
+    } else {
+        console.log("Invalid email");
+        document.querySelector("#failDialog").showModal();
+        return false;
+    }
+}
