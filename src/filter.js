e2a0a41a09389068978e27406ba9de357f0e6e95
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-12-15 22:42:26 -0500

    Add add filter process

--- src/filter.js
+++ src/filter.js
@@ -3,6 +3,7 @@
  */
 function createFilter() {
   const labelName = "Spam Shame Spam";
+  const email = "oliver...@gmail.com";
 
   // Check if label has already been created
   const currLabels = document.getElementsByClassName("nU");
@@ -29,7 +30,7 @@ function createFilter() {
     // Make label hidden from normal inbox
     // Short delay to wait for label to be added to DOM
     setTimeout(() => {
-      let hideIndex = Array.from(
+      const hideIndex = Array.from(
         document.getElementsByClassName("To")
       ).findIndex((x) => x.textContent.includes(labelName));
 
@@ -38,6 +39,45 @@ function createFilter() {
       hideButton.click();
     }, 2000);
   }
+
+  // Create filter for "dot email"
+  setTimeout(
+    () => {
+      // Go to Filters tab
+      const filtersTab = document.getElementsByClassName("f0")[4];
+      filtersTab.click();
+
+      setTimeout(() => {
+        // Create a new filter
+        const newFilterIndex = Array.from(
+          document.getElementsByClassName("sA")
+        ).findIndex((x) => x.textContent.includes("new filter"));
+        const newFilterButton = document
+          .getElementsByClassName("sA")
+          .item(newFilterIndex);
+        newFilterButton.click();
+
+        setTimeout(() => {
+          // Enter "dot email"
+          const toInput = document.getElementsByClassName("ZH nr aQf")[0];
+          toInput.value = email;
+
+          const createButton = document.getElementsByClassName("acM acN")[0];
+          createButton.click();
+
+          setTimeout(() => {
+            // Check apply label box
+            document.getElementsByClassName("btj")[3].checked = true;
+
+            alert(
+              `For security reasons you must do the rest. Click "Choose label..." and select ${labelName}. After, click "Create filter". Your filter is then created and you can exit this page.`
+            );
+          }, 500);
+        }, 1000);
+      }, 500);
+    },
+    !labelCreated ? 2200 : 0
+  );
 }
 
 createFilter();
